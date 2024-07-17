341feec434523d69a2b2f07888d83fdd14585392
Luísa Rocha <156859238+luisamrocha@users.noreply.github.com>
2024-07-17 17:01:50 -0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,53 @@
+"use strict";
+// let nome: string = "Luísa"
+// let idade: number = 25
+// let isEstudante :boolean = true
+// let a: number = 5 
+// let b: number = 6
+// let adicao: number =  a +b
+// let diminuicao: number = a -b
+// let multiplicacao: number = a *b
+// let divisao: number = a/b
+// console.log(adicao)
+// console.log(diminuicao)
+// console.log(multiplicacao)
+// console.log(divisao)
+// const nome = prompt("Digite seu nome: ");
+// if (nome !== null){
+//     console.log(`Olá , ${nome}! `)
+// }
+let numero = Number(prompt("Digite um número "));
+let tipo = numero % 2 == 0 ? "par" : "ímpar";
+console.log(tipo);
+// let day = Number(prompt("What is the day of the week? Write 1 if is Monday, 2 is Tuesday, 3 is wednesday, 4 thrusday, 5 friday,6 saturday, 7 is friday"))
+// let mensagem: string
+// switch (day){
+//     case 1:
+//         mensagem = "Hoje é segunda -feira"
+//         console.log(mensagem)
+//         break
+//     case 2:
+//         mensagem = "Hoje é terça-feira"
+//         console.log(mensagem)
+//         break
+//     case 3:
+//         mensagem = "Hoje é quarta-feira"
+//         console.log(mensagem)
+//         break
+//     case 4:
+//         mensagem = "Hoje é quinta-feira"
+//         console.log(mensagem)
+//         break
+//     case 5:
+//         mensagem = "Hoje é sexta-feira"
+//         console.log(mensagem)
+//         break
+//     case 6:
+//         mensagem ="Hoje é sabado"
+//         console.log(mensagem)
+//         break
+//     case 7:
+//         mensagem = "Hoje é domingo"
+//         console.log(mensagem)
+//         break
+// }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function hello(name) {
+    return "Seja bem-vindo ".concat(name);
+}
+console.log(hello("Luísa"));
